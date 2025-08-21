28593c674dc82d4e445907d061be418b51dd6ea8
Jason Madrid <jasonhnn6@gmail.com>
2025-08-20 19:06:39 -0600

    create class

---
+++ JasonMadrid_Examen1/Asignatura.cs
@@ -0,0 +1,10 @@
+﻿namespace JasonMadrid_Examen1
+{
+    public class Asignatura
+    {
+        public string NombreAsignatura { get; set; }
+        public string NombreDocente { get; set; }
+        public string Horario { get; set; }
+
+    }
+}
